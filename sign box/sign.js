e4478283cf916c0d2c18282b87cf189c6a6e8dd0
Malik Mammadov <malikgithub@gmail.com>
2023-12-08 20:12:51 +0400

    sign component are changed

--- sign box/sign.js
+++ sign box/sign.js
@@ -1,36 +1,35 @@
-let signParams = [];
-function signUpSubmit() {
-    signParams.length = 0;
-    let inputTags = document.getElementsByTagName("input");
-    for(let i = 0; i < inputTags.length; i++) {
-        signParams.push(inputTags[i].value);
-    }
-    console.log(signParams);
-    fetch('https://fakestoreapi.com/users', {
-        method: "POST",
-        body: JSON.stringify(
-            {
-                email: signParams[0],
-                username: signParams[1],
-                password: signParams[4],
-                name:{
-                    firstname: signParams[2],
-                    lastname: signParams[3]
-                },
-                address:{
-                    city:'kilcoole',
-                    street:'7835 new road',
-                    number:3,
-                    zipcode:'12926-3874',
-                    geolocation:{
-                            lat:'-37.3159',
-                            long:'81.1496'
-                    }
-                },
-                phone:'1-570-236-7033'
-            }
-        )
-    })
-    .then(response => response.json())
-    .then(data => console.log(data));
+let signBlockVisibility = true;
+let signPageTitle = document.querySelector("title");
+if(signBlockVisibility) {
+    document.getElementById("sign-in-div").style.display = "block";
+    document.getElementById("sign-up-div").style.display = "none";
+}else{
+    document.getElementById("sign-up-div").style.display = "block";
+    document.getElementById("sign-in-div").style.display = "none";
 }
+function signUpVisible(){
+    signBlockVisibility = false;
+    if(signBlockVisibility) {
+        document.getElementById("sign-in-div").style.display = "block";
+        document.getElementById("sign-up-div").style.display = "none";
+    }else{
+        document.getElementById("sign-up-div").style.display = "block";
+        document.getElementById("sign-in-div").style.display = "none";
+    }
+    signPageTitle.innerHTML = "Sign up";
+    let signUpIcon = document.querySelector("link[rel='shortcut icon']");
+    signUpIcon.setAttribute("href", "../imagesbox/icons8-зарегистрироваться-16.png");
+};
+function signInVisible() {
+    signBlockVisibility = true;
+    if(signBlockVisibility) {
+        document.getElementById("sign-in-div").style.display = "block";
+        document.getElementById("sign-up-div").style.display = "none";
+    }else{
+        document.getElementById("sign-up-div").style.display = "block";
+        document.getElementById("sign-in-div").style.display = "none";
+    }
+    signPageTitle.innerHTML = "Sign in";
+    let signInIcon = document.querySelector("link[rel='shortcut icon']");
+    signInIcon.setAttribute("href", "../imagesbox/icons8-войти-16.png")
+}; 
