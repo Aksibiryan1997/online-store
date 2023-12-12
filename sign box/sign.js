1fa3e80185669e5744bddd89ee21d1f646d845ca
Malik Mammadov <malikgithub@gmail.com>
2023-12-12 18:22:19 +0400

    add registration rules to sign.html

--- sign box/sign.js
+++ sign box/sign.js
@@ -53,15 +53,27 @@ function submitSignUp() {
     let signUpForm = document.getElementsByTagName("form")[1];
     let gmailVal = signUpForm.getElementsByTagName("input")[0].value;
     let passwordVal = signUpForm.getElementsByTagName("input")[1].value;
+    let signupPwdIssue = document.getElementById("signup-password-issue");
+    let signupGmailIssue = document.getElementById("signup-gmail-issue");
+    signupGmailIssue.style.display = "none";
+    signupPwdIssue.style.display = "none";
     let sameEmail = [];
     for(let i = 0; i < usersArr.length; i++) {
         if(usersArr[i].mail == gmailVal) {
             sameEmail.push(usersArr[i].mail);
         }
     }
-    if(sameEmail.length > 0 || !Boolean(gmailVal[0].match(/\w/g)) ||
-     !(gmailVal.includes("@")) || !gmailVal.endsWith("gmail.com")) {
-        if(sameEmail.length > 0) {
+    if(!Boolean(gmailVal[0].match(/\w/g)) ||
+     !(gmailVal.includes("@")) || !gmailVal.endsWith("gmail.com")
+     || Boolean(gmailVal.match(/\s/g))) {
+        signupGmailIssue.style.display = "block";
+        signupGmailIssue.innerHTML = "<ul class = 'pwd-issue'><li>" +
+         "- must begin with character a-z, A-Z, 0-9, including '_' .</li>" +
+         "<li>- must contain '@' symbol.</li>" +
+          "<li>- must not contain a space.</li>" + 
+          "<li>- must contain Latin alphabet.</li>" +
+         "<li>- must ends with 'gmail.com' .</li></ul>";
+        /*if(sameEmail.length > 0) {
             alert("There is an account with this email");
         }
         if(!Boolean(gmailVal[0].match(/\w/g))) {
@@ -72,11 +84,21 @@ function submitSignUp() {
         }
         if(!(gmailVal.endsWith("gmail.com"))) {
             alert("Email must ends with 'gmail.com'");
-        }
+        }*/
+    }else if(sameEmail.length > 0) {
+        signupGmailIssue.style.display = "block";
+        signupGmailIssue.innerHTML = "<ul class = 'pwd-issue'><li>" +
+         "<li>- There is an account with this email</li></ul>";
     }else if(passwordVal.length < 8 || Boolean(passwordVal.match(/\s/g)) ||
      !Boolean(passwordVal.match(/\d/g)) || !Boolean(passwordVal.match(/\W/g)) ||
      !Boolean(passwordVal.match(/[a-z]/g)) || !Boolean(passwordVal.match(/[A-Z]/g))) {
-        if(passwordVal.length < 8){
+        signupPwdIssue.style.display = "block";
+        signupPwdIssue.innerHTML = "<ul class = 'pwd-issue'><li>- at least 8 characters.</li>" +
+         "<li>- must not contain a space.</li>" + "<li>- at least one number.</li>" +
+          "<li>- at least one non-alphabetic and non-numeric character.</li>" +
+           "<li>- must contain Latin alphabet and at least one lowercase letter.</li>" + 
+           "<li>- at least one capital letter.</li></ul>";
+        /*if(passwordVal.length < 8){
             alert("The password must be at least 8 characters long");
         }
         if(Boolean(passwordVal.match(/\s/g))) {
@@ -94,13 +116,15 @@ function submitSignUp() {
         if(!Boolean(passwordVal.match(/[A-Z]/g))) {
             alert("The password must contain at least one capital letter");
             alert("the password must contain letters of the Latin alphabet");
-        }
+        }*/
     }else {
         gmailVal.trim();
         passwordVal.trim();
         let userExample = new User(gmailVal, passwordVal);
         usersArr.push(userExample);
         console.log(usersArr);
+        signInVisible();
+        setTimeout(()=>{alert("You have successfully registered. Enter your login details")}, 500);
     }
 }
 
@@ -108,13 +132,25 @@ function submitSignIn() {
     let signInForm = document.getElementsByTagName("form")[0];
     let gmailVal = signInForm.getElementsByTagName("input")[0].value;
     let passwordVal = signInForm.getElementsByTagName("input")[1].value;
+    let signinPwdIssue = document.getElementById("signin-password-issue");
+    let signinGmailIssue = document.getElementById("signin-gmail-issue");
+    signinGmailIssue.style.display = "none";
+    signinPwdIssue.style.display = "none";
     let rightUser = [];
     for(let i = 0; i < usersArr.length; i++) {
-       if(usersArr[i].mail == gmailVal && usersArr[i].passwrd == passwordVal) {
+       if(usersArr[i].mail == gmailVal /*&& usersArr[i].passwrd == passwordVal*/) {
         rightUser.push(usersArr[i]);
        }
     }
-    if(rightUser.length > 0) {
-        console.log("djambolat");
+    if(rightUser.length < 1) {
+        signinGmailIssue.style.display = "block";
+        signinGmailIssue.innerHTML = "<ul class = 'pwd-issue'><li>" + 
+        "<li>- You have entered an incorrect email address.</li></ul>";
+    }else if(rightUser.length > 0 && rightUser[0].passwrd != passwordVal) {
+        signinPwdIssue.style.display = "block";
+        signinPwdIssue.innerHTML = "<ul class = 'pwd-issue'><li>" + 
+        "<li>- You have entered an incorrect password.</li></ul>";
+    }else{
+        window.location.href = "../store.html";
     }
 }
